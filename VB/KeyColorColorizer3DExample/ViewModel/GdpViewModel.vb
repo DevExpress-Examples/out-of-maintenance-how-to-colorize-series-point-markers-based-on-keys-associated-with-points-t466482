edb0b58185d190595412e4f3a600f67b739de9e7
DevExpressExampleBot <fakeEmail>
2022-12-18 16:12:40 +0400

    Source auto update [skip ci]

--- VB/KeyColorColorizer3DExample/ViewModel/GdpViewModel.vb
+++ VB/KeyColorColorizer3DExample/ViewModel/GdpViewModel.vb
@@ -1,49 +1,53 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Collections.ObjectModel
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
 Imports System.Windows
 Imports System.Windows.Resources
 Imports System.Xml.Linq
 
 Namespace KeyColorColorizer3DExample.ViewModel
+
     Public Class Gdp
-        Private privateCountry As String
-        Public Property Country() As String
+
+        Private _Country As String, _Year As Integer, _Value As Double, _Region As String
+
+        Public Property Country As String
             Get
-                Return privateCountry
+                Return _Country
             End Get
+
             Private Set(ByVal value As String)
-                privateCountry = value
+                _Country = value
             End Set
         End Property
-        Private privateYear As Integer
-        Public Property Year() As Integer
+
+        Public Property Year As Integer
             Get
-                Return privateYear
+                Return _Year
             End Get
+
             Private Set(ByVal value As Integer)
-                privateYear = value
+                _Year = value
             End Set
         End Property
-        Private privateValue As Double
-        Public Property Value() As Double
+
+        Public Property Value As Double
             Get
-                Return privateValue
+                Return _Value
             End Get
+
             Private Set(ByVal value As Double)
-                privateValue = value
+                _Value = value
             End Set
         End Property
-        Private privateRegion As String
-        Public Property Region() As String
+
+        Public Property Region As String
             Get
-                Return privateRegion
+                Return _Region
             End Get
+
             Private Set(ByVal value As String)
-                privateRegion = value
+                _Region = value
             End Set
         End Property
 
@@ -56,37 +60,41 @@ Namespace KeyColorColorizer3DExample.ViewModel
     End Class
 
     Public Class GdpViewModel
-        Private privateGdps As IEnumerable(Of Gdp)
-        Public Property Gdps() As IEnumerable(Of Gdp)
+
+        Private _Gdps As IEnumerable(Of KeyColorColorizer3DExample.ViewModel.Gdp)
+
+        Public Property Gdps As IEnumerable(Of Gdp)
             Get
-                Return privateGdps
+                Return _Gdps
             End Get
+
             Private Set(ByVal value As IEnumerable(Of Gdp))
-                privateGdps = value
+                _Gdps = value
             End Set
         End Property
 
         Public Sub New()
-            Gdps = (New GdpResourceLoader("Data/GdpStatistics.xml")).Load()
+            Gdps = New GdpResourceLoader("Data/GdpStatistics.xml").Load()
         End Sub
     End Class
 
     Public Class GdpResourceLoader
-        Private Property Filepath() As String
+
+        Private Property Filepath As String
 
         Public Sub New(ByVal filepath As String)
             Me.Filepath = filepath
         End Sub
 
         Public Function Load() As IEnumerable(Of Gdp)
-            Dim result As New Collection(Of Gdp)()
-
-            Dim resourceUri As New Uri(Filepath, UriKind.RelativeOrAbsolute)
+            Dim result As Collection(Of Gdp) = New Collection(Of Gdp)()
+            Dim resourceUri As Uri = New Uri(Filepath, UriKind.RelativeOrAbsolute)
             Dim resourceInfo As StreamResourceInfo = Application.GetResourceStream(resourceUri)
             Dim xDoc As XDocument = XDocument.Load(resourceInfo.Stream)
             For Each xGdp In xDoc.Element("Statistics").Elements("Gdp")
-                result.Add(New Gdp(country:= xGdp.Attribute("Country").Value, year:= Convert.ToInt32(xGdp.Attribute("Year").Value), value:= Convert.ToDouble(xGdp.Attribute("Value").Value), region:= xGdp.Attribute("Region").Value))
-            Next xGdp
+                result.Add(New Gdp(country:=xGdp.Attribute("Country").Value, year:=Convert.ToInt32(xGdp.Attribute("Year").Value), value:=Convert.ToDouble(xGdp.Attribute("Value").Value), region:=xGdp.Attribute("Region").Value))
+            Next
+
             Return result
         End Function
     End Class
